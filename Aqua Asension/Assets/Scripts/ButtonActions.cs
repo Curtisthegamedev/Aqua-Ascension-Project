7214ed1eb2fefe622463d5bfa8764d5f404c0f3e
Curtisthegamedev <curtis.squire@rogers.com>
2020-10-01 15:04:56 -0700

    Created Menus For Joining Game Lobbys

---
+++ Aqua Asension/Assets/Scripts/ButtonActions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonActions : MonoBehaviour
+{
+    //Variables 
+    [SerializeField] GameObject canvasMainMenu;
+    [SerializeField] GameObject canvasCreateLobby;
+    [SerializeField] GameObject canvasJoinLobby;
+    public void CreateLobby()
+    {
+        canvasMainMenu.SetActive(false);
+        canvasCreateLobby.SetActive(true);
+    }
+
+    public void enterLobby()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+
+    public void JoinLobby()
+    {
+        canvasMainMenu.SetActive(false);
+        canvasJoinLobby.SetActive(true); 
+    }
+
+    public void Join()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+}
---
+++ Aqua Asension/Assets/Scripts/GenerateLobbyCode.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro; 
+
+public class GenerateLobbyCode : MonoBehaviour
+{
+    private string lobbyCode;
+    private int lobbyCodeNumber;
+    [SerializeField] TextMeshProUGUI textDisplay; 
+
+    public void GenerateRandomLobbyCode()
+    {
+        lobbyCodeNumber = Random.Range(1000, 9999);
+        lobbyCodeNumber.ToString(); 
+        textDisplay.SetText("Number: " + lobbyCodeNumber); 
+    }
+
+    private void Start()
+    {
+        GenerateRandomLobbyCode(); 
+    }
+}
