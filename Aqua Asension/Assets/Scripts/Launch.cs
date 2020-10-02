a5d4df6cbad8a3ffe1a74826ea506a9738b1f60e
Curtisthegamedev <curtis.squire@rogers.com>
2020-10-02 06:22:23 -0700

    Connected Game To Internet

---
+++ Aqua Asension/Assets/Scripts/Launch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun; 
+
+public class Launch : MonoBehaviourPunCallbacks
+{
+    [SerializeField] GameObject CanvasMainMenu; 
+    private void Start()
+    {
+        Debug.Log("Attempting to connect to master..."); 
+        PhotonNetwork.ConnectUsingSettings(); 
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Joined Master"); 
+        PhotonNetwork.JoinLobby(); 
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("joined lobby");
+        CanvasMainMenu.SetActive(true); 
+    }
+}
