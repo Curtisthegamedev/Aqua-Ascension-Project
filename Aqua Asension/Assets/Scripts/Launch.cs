8da667968c65293a27b0c7e6340f4bf819f267c5
Curtisthegamedev <curtis.squire@rogers.com>
2020-10-02 17:14:21 -0700

    changed launch

--- Aqua Asension/Assets/Scripts/Launch.cs
+++ Aqua Asension/Assets/Scripts/Launch.cs
@@ -1,26 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Photon.Pun; 
+using Photon.Pun;
+using TMPro;
 
 public class Launch : MonoBehaviourPunCallbacks
 {
-    [SerializeField] GameObject CanvasMainMenu; 
+
+
+    [SerializeField] GameObject CanvasMainMenu;
+    [SerializeField] GameObject CanvasLoad;
+    [SerializeField] GameObject GameNumbers;
+
     private void Start()
     {
-        Debug.Log("Attempting to connect to master..."); 
-        PhotonNetwork.ConnectUsingSettings(); 
+        Debug.Log("Attempting to connect to master...");
+        PhotonNetwork.ConnectUsingSettings();
     }
 
     public override void OnConnectedToMaster()
     {
-        Debug.Log("Joined Master"); 
-        PhotonNetwork.JoinLobby(); 
+        Debug.Log("Joined Master");
+        PhotonNetwork.JoinLobby();
     }
 
     public override void OnJoinedLobby()
     {
         Debug.Log("joined lobby");
-        CanvasMainMenu.SetActive(true); 
+        CanvasLoad.SetActive(false);
+        CanvasMainMenu.SetActive(true);
+    }
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(GameNumbers.GetComponentInParent<TextMeshPro>());
+    }
+
+    public override void OnJoinedRoom()
+    {
+
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        base.OnCreateRoomFailed(returnCode, message);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        base.OnJoinRandomFailed(returnCode, message);
     }
 }
