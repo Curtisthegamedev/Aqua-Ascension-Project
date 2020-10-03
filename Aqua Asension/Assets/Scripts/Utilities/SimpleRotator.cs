e5bd9acb3bfa8db02f9cd931bed148c427b9f389
Juriel Garcia <jurielgarcia2010@gmail.com>
2020-10-03 02:45:43 -0400

    Pretified Menu Screen

---
+++ Aqua Asension/Assets/Scripts/Utilities/SimpleRotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace AquaAscension.Utils
+{
+    public class SimpleRotator : MonoBehaviour
+    {
+        [SerializeField] private Vector3 rotator = Vector3.zero;
+        [SerializeField] private float drag = 0.01f;
+
+        Vector3 rotation = Vector3.zero;
+
+        private void Update() {
+            rotation += Vector3.Lerp(Vector3.zero, rotator, Time.timeSinceLevelLoad * drag) * Time.deltaTime;
+            transform.localRotation = Quaternion.Euler(rotation);    
+        }
+    }
+}
