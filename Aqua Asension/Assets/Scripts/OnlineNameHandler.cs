5012bd2f9b327160e6aab6773ba6fb207a342297
Curtisthegamedev <curtis.squire@rogers.com>
2020-10-02 20:05:35 -0700

    Began Working on online nickname system

---
+++ Aqua Asension/Assets/Scripts/OnlineNameHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Realtime;
+using Photon.Pun;
+
+public class OnlineNameHandler : MonoBehaviour
+{
+    [SerializeField] Button JoinButton;
+    [SerializeField] Button CreateLobbyButton;
+    [SerializeField] TMP_InputField PlayerOnlineNickname;
+    private bool PlayerNameSet = false; 
+
+    private void Update()
+    {
+        JoinButton.interactable = !string.IsNullOrEmpty(PlayerOnlineNickname.text);
+        CreateLobbyButton.interactable = !string.IsNullOrEmpty(PlayerOnlineNickname.text);
+    }
+
+    public void SetPlayerName()
+    {
+        PhotonNetwork.NickName = PlayerOnlineNickname.text; 
+
+    }
+
+
+}
