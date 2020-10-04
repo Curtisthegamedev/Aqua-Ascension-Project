eb5f481a3a9896d97fd136786d6fdbea7895d9f2
Curtisthegamedev <curtis.squire@rogers.com>
2020-10-03 23:10:59 -0700

    Created New Scene And Began Work on movement

--- Aqua Asension/Assets/Scripts/PlayerMove.cs
+++ Aqua Asension/Assets/Scripts/PlayerMove.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    
+    public CharacterController controller;
+    private float smoothTime = 0.2f;
+    private float SmoothVelocity; 
+    private float speed = 5;
+    [SerializeField] Transform cam; 
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 dir = new Vector3(horizontal, 0.0f, vertical).normalized;
+        
+        if (dir.magnitude >= 0.1f)
+        {
+            float angle = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float SmoothedAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref SmoothVelocity, smoothTime); 
+
+            transform.rotation = Quaternion.Euler(0.0f, SmoothedAngle, 0.0f);
+            Vector3 moveDir = Quaternion.Euler(0.0f, angle, 0.0f) * Vector3.forward;  
+
+            controller.Move(dir * speed * Time.deltaTime); 
+        }
+    }
 }
