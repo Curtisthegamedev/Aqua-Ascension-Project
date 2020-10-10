cf071ee584cc03ec710cf54c37557106c7177863
Curtisthegamedev <curtis.squire@rogers.com>
2020-10-10 15:50:20 -0700

    Updates to the player movement script

---
+++ Aqua Asension/Assets/Scripts/Physics/PlayerMovement/PlayerMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private float speed = 10;
+    private float gravity = 8.5f;
+    private float jumpForce = 5.0f; 
+    private float verticalVelocity; 
+    private CharacterController controller; 
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+        controller = this.gameObject.GetComponent<CharacterController>(); 
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        if(controller.isGrounded)
+        {
+            verticalVelocity = -gravity * Time.deltaTime; 
+            if(Input.GetButton("Jump"))
+            {
+                verticalVelocity = jumpForce; 
+            }
+            
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime; 
+        }
+        Vector3 MoveVector = new Vector3(vertical * speed, verticalVelocity, -horizontal * speed);
+        controller.Move(MoveVector * Time.deltaTime); 
+    }
+}
