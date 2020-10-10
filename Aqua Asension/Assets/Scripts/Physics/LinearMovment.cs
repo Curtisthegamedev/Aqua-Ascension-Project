40550675b6f798296ab2c9099c5b906b1add6591
Curtisthegamedev <curtis.squire@rogers.com>
2020-10-09 23:41:27 -0700

    Created the first assinment for Physics

--- Aqua Asension/Assets/Scripts/Physics/LinearMovment.cs
+++ Aqua Asension/Assets/Scripts/Physics/LinearMovment.cs
@@ -6,16 +6,15 @@ public class LinearMovment : MonoBehaviour
 {
     //This will reprisent endpointOne for Par 2 and endpointTwo for Par.
     [SerializeField] Transform endpoint;
-    private float angularVel = 45.0f; 
-
+    [SerializeField] float angularVel = 45.0f;
+    private Rigidbody r;
     private float velosity = 2.5f;
 
 
     // Start is called before the first frame update
     void Start()
     {
-
-
+        r = GetComponent<Rigidbody>();
     }
 
 
@@ -24,10 +23,10 @@ public class LinearMovment : MonoBehaviour
     void Update()
     {
         //Movement physics
-        transform.position = Vector3.MoveTowards(transform.position, endpoint.transform.position, Time.deltaTime * velosity); 
+        transform.position = Vector3.MoveTowards(transform.position, endpoint.transform.position, Time.deltaTime * velosity);
 
         //Rotation phisics
-
+        transform.Rotate(0, angularVel * Time.deltaTime, 0, Space.Self); 
 
     }
 }
