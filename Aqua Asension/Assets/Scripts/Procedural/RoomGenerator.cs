b36ebd4b1016d732e51d14f3db4605fff249c17c
Juriel Garcia <jurielgarcia2010@gmail.com>
2020-10-16 07:21:58 -0400

    Progrids, Polybrush Samples & Procedural Start

---
+++ Aqua Asension/Assets/Scripts/Procedural/RoomGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This class creates rooms by procedurally linking components together.
+
+public enum Height
+{
+    Low, Medium, High
+}
+
+public struct RoomComponent
+{
+    public Height Height{get; set;}
+}
+
+[System.Serializable]
+public class Room 
+{
+    public Height height;
+    public Height verticalHeight;
+    public Height Height {get => height; set => height = value;}
+    public Height VerticalHeight {get; set;} 
+}
+
+public class RoomGenerator : MonoBehaviour
+{
+    
+}
---
+++ Aqua Asension/Assets/Scripts/Procedural/TowerGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ConnectionPoint 
+{
+    Stairs, Elevator, JumpPad
+}
+
+// This class generates a tower by procedurally linking rooms together
+public class TowerGenerator : MonoBehaviour
+{
+    
+    public static SortedSet<Room> rooms;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
