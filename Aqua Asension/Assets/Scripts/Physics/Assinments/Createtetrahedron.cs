20cb18e7538c46a034c070da606d95f9abaeac48
Curtisthegamedev <curtis.squire@rogers.com>
2020-10-21 23:35:03 -0700

    Created Script to print points for physics assinment 2

---
+++ Aqua Asension/Assets/Scripts/Physics/Assinments/Createtetrahedron.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Createtetrahedron : MonoBehaviour
+{
+    Mesh mesh;
+    Vector3[] verts;
+    int[] triangles;
+
+    private void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh; 
+
+
+    }
+
+    private void MakeTetrahedron()
+    {
+        verts = new Vector3[]
+        {
+            new Vector3 (0, 0, 0),
+            new Vector3 (0, 0, 1), 
+            new Vector3 (1, 0, 0)
+        };
+
+        triangles = new int[]
+        {
+            0, 1, 2
+        };
+    }
+
+    private void UpdateMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = verts;
+        mesh.triangles = triangles; 
+    }
+}
---
+++ Aqua Asension/Assets/Scripts/Physics/Assinments/PrintVertexPositions.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintVertexPositions : MonoBehaviour
+{
+    Mesh mesh;
+    Vector3 vertexPos1;
+    Vector3 vertexPos2;
+    Vector3 vertexPos3;
+    //Vector3 vertexPos4;
+    Vector3 vertexPos5;
+    //Vector3 vertexPos6;
+    //Vector3 vertexPos7;
+    Vector3 vertexPos8;
+    Vector3 vertexPos9; 
+
+    private void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+        vertexPos1 = mesh.vertices[0];
+        vertexPos2 = mesh.vertices[1];
+        vertexPos3 = mesh.vertices[2];
+        //vertexPos4 = mesh.vertices[3];
+        vertexPos5 = mesh.vertices[4];
+        //vertexPos6 = mesh.vertices[5];
+        //vertexPos7 = mesh.vertices[6];
+        //vertexPos8 = mesh.vertices[7];
+        vertexPos9 = mesh.vertices[8]; 
+
+        Debug.Log("Vertex One is " + transform.TransformPoint(vertexPos1));
+        Debug.Log("Vertex Two is " + transform.TransformPoint(vertexPos2));
+        Debug.Log("vertex Three is " + transform.TransformPoint(vertexPos3));
+        Debug.Log("vertex four is " + transform.TransformPoint(vertexPos5));
+        Debug.Log("vertex five is " + transform.TransformPoint(vertexPos9));
+    }
+}
