80fe86fba98ab6ee38329f677da844fe6ea7940a
Curtisthegamedev <curtis.squire@rogers.com>
2020-11-02 23:54:34 -0800

    networking

---
+++ Aqua Asension/Assets/Scripts/Spawning.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawning : MonoBehaviour
+{
+    public Transform[] spawnpoints;
+    public static Spawning SpawnScript; 
+}
