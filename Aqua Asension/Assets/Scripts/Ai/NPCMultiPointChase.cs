8745fd044d42ce33e2d35384894a67366e091250
Gamegar <43215912+Gamegar@users.noreply.github.com>
2020-11-05 20:44:42 -0500

    multi point movment for ai

---
+++ Aqua Asension/Assets/Scripts/Ai/NPCMultiPointChase.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+
+public class NPCMultiPointChase : MonoBehaviour
+{
+    [SerializeField] bool StallMove;
+
+    [SerializeField] float StallTime = 4f;
+
+    [SerializeField] float Switch = 0.1f;
+
+    [SerializeField] List<Waypoint> patrolpoint; 
+
+    NavMeshAgent navMeshAgent;
+
+
+    int currentPatrolIndex;
+    bool move;
+    bool Wait;
+    bool patrolF;
+    float waitimmer;
+
+    // Start is called before the first frame update
+    public void  Start()
+    {
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
+        if(navMeshAgent == null)
+        {
+            Debug.LogError("No nav mesh on. " + gameObject.name + " wtf.");
+        }
+        else
+        {
+            if (patrolpoint != null && patrolpoint.Count >= 2)
+            {
+                currentPatrolIndex = 0;
+                SetDestination();
+            }
+            else
+            {
+
+                Debug.Log("Not enough points");
+            }
+        }
+
+
+    }
+
+    // Update is called once per frame
+   public void Update()
+    {
+       if(move && navMeshAgent.remainingDistance <= 1.0f)
+        {
+
+            move = false;
+
+            if(StallMove)
+            {
+
+                Wait = true;
+               waitimmer = 0f;
+
+            }
+            else
+            {
+                ChangePatrolPoint();
+                SetDestination();
+            }
+
+        }
+        if (Wait)
+        {
+            waitimmer += Time.deltaTime;
+            if(waitimmer >= StallTime)
+            {
+                Wait = false;
+
+                ChangePatrolPoint();
+                SetDestination();
+            }
+        }
+    }
+
+    private void SetDestination()
+    {
+        if(patrolpoint != null)
+        {
+
+            Vector3 targetVector = patrolpoint[currentPatrolIndex].transform.position;
+            navMeshAgent.SetDestination(targetVector);
+            move = true;
+        }
+
+    }
+
+    private void ChangePatrolPoint()
+    {
+        if (UnityEngine.Random.Range(0f,1f)<= Switch)
+        {
+            patrolF = !patrolF;
+        }
+        if (patrolF)
+        {
+            currentPatrolIndex = (currentPatrolIndex + 1) % patrolpoint.Count;
+        }
+        else
+        {
+            if(--currentPatrolIndex < 0)
+            {
+                currentPatrolIndex = patrolpoint.Count - 1;
+            }
+        }
+
+    }
+}
---
+++ Aqua Asension/Assets/Scripts/Ai/Waypoint.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    [SerializeField] protected float debugDrawRadious = 1.0f;
+    public virtual void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, debugDrawRadious);
+    }
+}
