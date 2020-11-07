1d1634d1b97b4f35f928a8571881d173ab6017ee
Curtisthegamedev <curtis.squire@rogers.com>
2020-11-07 13:14:27 -0800

    Mutiple players in networking scene

--- Aqua Asension/Assets/Scripts/Physics/PlayerMovement/PlayerMove.cs
+++ Aqua Asension/Assets/Scripts/Physics/PlayerMovement/PlayerMove.cs
@@ -1,40 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun; 
 
-public class PlayerMove : MonoBehaviour
+public class PlayerMove : MonoBehaviourPunCallbacks
 {
-    private float speed = 10;
+    private float speed = 5;
     private float gravity = 8.5f;
-    private float jumpForce = 5.0f; 
-    private float verticalVelocity; 
-    private CharacterController controller; 
+    private float jumpForce = 5.0f;
+    private Vector3 verticalVelocity;
+    [SerializeField] Transform camTransform;
+    [SerializeField] GameObject WaterEffect;
+    //this bullet prefab is temporary and should be replaced with water later on. 
+    [SerializeField] GameObject tempBulletPrefab;
+    private CharacterController controller;
     Rigidbody rb;
+    float smoothVel;
 
     private void Start()
     {
         rb = this.gameObject.GetComponent<Rigidbody>();
-        controller = this.gameObject.GetComponent<CharacterController>(); 
+        controller = this.gameObject.GetComponent<CharacterController>();
     }
-
     private void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        if(controller.isGrounded)
+        if(photonView.IsMine)
         {
-            verticalVelocity = -gravity * Time.deltaTime; 
-            if(Input.GetButton("Jump"))
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+            Vector3 MoveVector = new Vector3(horizontal, 0.0f, vertical).normalized;
+            if (MoveVector.magnitude >= 0.1)
             {
-                verticalVelocity = jumpForce; 
+                float angle = Mathf.Atan2(MoveVector.x, MoveVector.y) * Mathf.Rad2Deg + camTransform.eulerAngles.y;
+                float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref smoothVel, 0.1f);
+                Vector3 moveDirection = Quaternion.Euler(0.0f, angle, 0.0f) * Vector3.forward;
+                transform.rotation = Quaternion.Euler(0.0f, smoothAngle, 0.0f);
+                controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+
             }
-            
+
+            if (controller.isGrounded)
+            {
+                if (Input.GetButton("Jump"))
+                {
+                    verticalVelocity.y = jumpForce;
+                }
+
+            }
+            else
+            {
+                verticalVelocity.y += -gravity * Time.deltaTime;
+            }
+            controller.Move(MoveVector * Time.deltaTime);
+            controller.Move(verticalVelocity * Time.deltaTime);
         }
-        else
-        {
-            verticalVelocity -= gravity * Time.deltaTime; 
-        }
-        Vector3 MoveVector = new Vector3(vertical * speed, verticalVelocity, -horizontal * speed);
-        controller.Move(MoveVector * Time.deltaTime); 
     }
 }
