8dd511cc63186ea98d0618026465f2c82dd73e10
Curtisthegamedev <curtis.squire@rogers.com>
2020-11-07 13:14:27 -0800

    Mutiple players in networking scene

--- Aqua Asension/Assets/Scripts/Managers/PhotonPlayeManager.cs
+++ Aqua Asension/Assets/Scripts/Managers/PhotonPlayeManager.cs
@@ -6,8 +6,33 @@ using Photon.Pun;
 public class PhotonPlayeManager : MonoBehaviour
 {
     [SerializeField] GameObject PrefabPlayer;
+    private bool firstPlayerSpawned = false;
+    private bool secondPlayerSpawned = false;
+    private bool ThirdPlayerSpawned = false; 
+    [SerializeField] Transform spawnLocationOne;
+    [SerializeField] Transform spawnLocationTwo;
+    [SerializeField] Transform spawnLocationThree;
+    [SerializeField] Transform spawnLocationFour; 
     private void Start()
     {
-        PhotonNetwork.Instantiate(PrefabPlayer.name, new Vector3(70, -2, -39), Quaternion.identity); 
+        if(!firstPlayerSpawned && !secondPlayerSpawned && !ThirdPlayerSpawned)
+        {
+            PhotonNetwork.Instantiate(PrefabPlayer.name, new Vector3(70, -2, -39), Quaternion.identity);
+            firstPlayerSpawned = true; 
+        }
+        else if(firstPlayerSpawned && !secondPlayerSpawned && !ThirdPlayerSpawned)
+        {
+            PhotonNetwork.Instantiate(PrefabPlayer.name, new Vector3(70, -4, -56), Quaternion.identity);
+            secondPlayerSpawned = true; 
+        }
+        else if(firstPlayerSpawned && secondPlayerSpawned && !ThirdPlayerSpawned)
+        {
+            PhotonNetwork.Instantiate(PrefabPlayer.name, new Vector3(58, -2, -56), Quaternion.identity);
+            ThirdPlayerSpawned = true; 
+        }
+        else
+        {
+            PhotonNetwork.Instantiate(PrefabPlayer.name, new Vector3(57, -2, -38), Quaternion.identity); 
+        }
     }
 }
