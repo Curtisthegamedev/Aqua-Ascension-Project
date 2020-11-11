cfb297bd2544d04c9690abb8c7451fd7f592e7a7
Curtisthegamedev <curtis.squire@rogers.com>
2020-11-10 18:19:22 -0800

    Added new scene for testing online copetative modes

---
+++ Aqua Asension/Assets/Scripts/Physics/PlayerMovement/Gun.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] Transform FirePoint;
+    [SerializeField] GameObject TempBullet;
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(TempBullet, FirePoint.position, FirePoint.rotation); 
+        }
+    }
+}
