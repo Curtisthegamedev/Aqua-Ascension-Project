892f0ebd66c5ce83227250ae0a92730545bdcc71
darkwolf4747 <54948380+darkwolf4747@users.noreply.github.com>
2020-11-23 18:38:16 -0500

    added scripts
    forgot em. lol

---
+++ Aqua Asension/Assets/Scripts/Combo Lock/LockControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockControl : MonoBehaviour
+{
+    private int[] result, correctCombination;
+    private void Start()
+    {
+        result = new int[] { 0, 0, 0 };
+        correctCombination = new int[] { 6, 6, 6 }; //haha, this is temporary, we will make it randomly generate every time the room is generated with this puzzle!
+        Rotate.Rotated += CheckResults;
+    }
+
+    private void CheckResults(string wheelName, int number)
+    {
+        switch (wheelName)
+        {
+            case "Wheel1":
+                result[0] = number;
+                break;
+
+            case "Wheel2":
+                result[1] = number;
+                break;
+
+            case "Wheel3":
+                result[2] = number;
+                break;
+
+            case "Wheel4":
+                result[3] = number;
+                break;
+        }
+        if (result[0] == correctCombination[0] && result[1] == correctCombination[1] && result[2] == correctCombination[2] && result[3] == correctCombination[3])
+        {
+            Debug.Log("Correct Combination has been entered!"); //this is also temporary, this would trigger the event of providing the player with the weapon upgrade or unlocking the next level!
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Rotate.Rotated -= CheckResults;
+    }
+}
