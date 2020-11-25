47020da0721e2fcd27a2da4937e3e5510daa4091
Curtisthegamedev <curtis.squire@rogers.com>
2020-11-24 19:26:01 -0800

    Added Timer for deathmatch mode

--- Aqua Asension/Assets/Scripts/Health.cs
+++ Aqua Asension/Assets/Scripts/Health.cs
@@ -2,12 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-using UnityEngine.UI; 
+using UnityEngine.UI;
+using UnityEngine.Events; 
 
 public class Health : MonoBehaviourPunCallbacks, IPunObservable
 {
     public int myHealth = 100;
-    [SerializeField] Image healthBar; 
+    [SerializeField] Image healthBar;
+    private Transform SpawnContainer;
+    [SerializeField] List<Transform> spawnPoints = new List<Transform>();
+    UnityEvent <GameObject>respawnEvent; 
+
+    public void Initialize(UnityEvent <GameObject>RespawnEvent)
+    {
+        respawnEvent = RespawnEvent;
+    }
+
+    private void Start()
+    {
+        SpawnContainer = GameObject.FindGameObjectWithTag("SpawnContainer").transform; 
+        foreach (Transform child in SpawnContainer)
+            spawnPoints.Add(child); 
+    }
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
@@ -29,8 +45,14 @@ public class Health : MonoBehaviourPunCallbacks, IPunObservable
         healthBar.fillAmount = myHealth; 
         if(myHealth <= 0)
         {
-            
+            respawnEvent?.Invoke(this.gameObject);
+            Debug.Assert(respawnEvent != null); 
         }
+    }
+
+    public void ResetHealth()
+    {
+        myHealth = 100; 
     }
 
     
--- Aqua Asension/Assets/Scripts/Managers/PhotonPlayerManager.cs
+++ Aqua Asension/Assets/Scripts/Managers/PhotonPlayerManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using UnityEngine.Events; 
 
 public class PhotonPlayerManager : MonoBehaviour
 {
@@ -26,12 +27,20 @@ public class PhotonPlayerManager : MonoBehaviour
     [Header("Debug")]
     [SerializeField] List<Transform> spawnPoints = new List<Transform>();
 
+    UnityEvent<GameObject> respawnPlayerEvent; 
+
     private void Start()
     {
         Debug.Assert(PhotonNetwork.IsConnectedAndReady && PhotonNetwork.InRoom);
 
         foreach (Transform child in spawnPointsContainer)
             spawnPoints.Add(child);
+
+        if(respawnPlayerEvent == null)
+        {
+            respawnPlayerEvent = new UnityEvent<GameObject>(); 
+        }
+        respawnPlayerEvent.AddListener(Respawn); 
 
         Spawn();
     }
@@ -44,5 +53,17 @@ public class PhotonPlayerManager : MonoBehaviour
         var position = spawn.position + new Vector3(0, offset, 0);
 
         GameObject player = PhotonNetwork.Instantiate(PrefabPlayer.name, position, Quaternion.identity);
+        //player.GetComponent<Health>().Initialize(respawnPlayerEvent); 
+    }
+
+    protected void Respawn(GameObject player)
+    {
+        Debug.Assert(spawnPoints.Count > 0, this); // Need to have atleast one spawn point
+
+        var spawn = spawnPoints[Random.Range(0, spawnPoints.Count)];
+        var position = spawn.position + new Vector3(0, offset, 0);
+
+        player.transform.position = position;
+        player.GetComponent<Health>().ResetHealth(); 
     }
 }
