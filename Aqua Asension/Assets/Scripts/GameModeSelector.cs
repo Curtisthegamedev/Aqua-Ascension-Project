9da4d47cec35c3b0558c7e00ea90c7f3371a866d
Garciaj007 <jurielgarcia2010@gmail.com>
2020-12-02 00:00:45 -0500

    Gamemode Specific Button Action
    Each button has a Gamemode Selector which links which scene to load henceforth. And is currently set to test scene.

--- Aqua Asension/Assets/Scripts/GameModeSelector.cs
+++ Aqua Asension/Assets/Scripts/GameModeSelector.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
+using TMPro;
 
 namespace AquaAscension
 {
     public enum GameMode
     {
-        Arena, TeamedArena, Race, BattleRoyal
+        Arena, TeamedArena, Race, BattleRoyal, TestPuzzle
     }
 
     public class GameModeSelector : MonoBehaviour
     {
         public GameMode mode;
+
+        void Start()
+        {
+            var textComponent = GetComponentInChildren<TextMeshProUGUI>();
+            textComponent.text = "Start " + mode.ToString("F");
+        }
     }
 }
