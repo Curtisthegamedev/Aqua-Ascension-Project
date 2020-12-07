ff51f0a1cb94bc7b27187fa00b9a0b5d567d4a76
Curtisthegamedev <curtis.squire@rogers.com>
2020-12-07 01:37:20 -0500

    Began Working on Boyancy

---
+++ Aqua Asension/Assets/Scripts/Physics/Bouyancy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouyancy : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float LiquidDesity = 3000.0f;
+    private float volume = 1.0f;
+    private Vector3 Boyancy; 
+
+    private void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Boyancy = LiquidDesity * volume; 
+    }
+}
